f7985e14e190f7f1a8352a9a4f51419a639f514a
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-11 02:08:53 +0300

    sestos uzduoties failas

---
+++ src/Uzduotis6.java
@@ -0,0 +1,12 @@
+/*
+1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
+2. Susikurti papildomą funkciją, kuri per parametrus gauna List<Boolean> loginiai ir tą boolean list'ą konvertuoja į dvejatainį skaičių. Atsiminkime, kad booleanai tai yra tiesiog true ir false arba dar kitaip tai tiesiog 1 ir 0. Tai galima eiti per visus list'o narius ir tikrinti ar jie true/false ir tada atitinkamai sumuoti vienetus ir nulius prie atsakymo String kintamojo. Pvz., jeigu turime list = {true, true, false, true, false, false, true, true}, tai dvejatainis skaičius būtų: 1101001, nes true = 1, o false = 0
+
+ */
+public class Uzduotis6 {
+    public static void main(String[] args) {
+
+    }
+
+
+}
