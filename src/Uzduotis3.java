79fdfe743bb082fb6c6c8c655900af14588bbbce
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-11 01:25:49 +0300

    Nuskaitymo funkcija

--- src/Uzduotis3.java
+++ src/Uzduotis3.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /*
 1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
 2. Susikurti papildomą funkciją, kuri per parametrus gauna List<Integer> listas ir suranda visų listo neigiamų skaičių sumą.
@@ -5,8 +8,27 @@
  */
 public class Uzduotis3 {
     public static void main(String[] args) {
+        ArrayList<Integer> skaiciai = nuskaitytiLista();
+
 
     }
+
+    /**
+     * Nuskaito listą iš konsolės
+     * @return listą
+     */
+    public static ArrayList<Integer> nuskaitytiLista() {
+        Scanner skait = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println("Įveskite kiek bus skaičių: ");
+        int n = skait.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("Įveskite " + i + "-ąjį skaičių:");
+            list.add(skait.nextInt());
+        }
+        return list;
+    }
+
 
 
 }
