331e42cb2a60ebf90a5782fd7a0f46fcd8d5813c
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-11 02:44:31 +0300

    devintos uzduoties failas

---
+++ src/Uzduotis9.java
@@ -0,0 +1,13 @@
+/*
+1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
+
+Kartais vidurkis nėra pats geriausias kriterijus. Pavyzdžiui, jeigu turime įmonės algas {1000, 1000, 1000, 5000} tai vidurkis šios įmonės gal ir atrodytų patraukliai (2000), tačiau realybė būtų tokia, jog visi gauna 1000, o tik vienintelis (direktorius) gauna 5000. Kad šią problemą (kai extreme point / outlier vertės per daug įtakoja vidurkį) yra "sukurta" mediana. Apie ją pasiskaityti daugiau galite internete: https://lt.wikipedia.org/wiki/Mediana Trumpai tariant, kai surikiuojame skaičius (algas) tai vidurinė alga būtų mediana. Jeigu yra dvi vidurinės tai tų dviejų vidurkis yra mediana. Tai šiuo atveju turime dvi vidurines algas (antrą ir trečią), jos abi yra 1000, tad jų bendras vidurkis irgi bus 1000. Tad mediana = 1000. Kitokiais žodžiais, jeigu mediana yra 1000, tai reiškia, jog pusė darbuotojų gauna arba lygiai 1000 arba dar mažesnį atlyginimą ir tai padeda susidaryti išsamesnę nuomonę apie įmonės atlyginimus.
+
+2. Sukurkite papildomą funkciją, kuri  gauna ArrayList<Double> list ir suranda bei returnina double mediana. TUREKITE OMENYJE, kad prieš ieškant medianos, listą reikia SURIKIUOTI. (rikiavimą galite daryti tos papildomos funkcijos viduje)
+
+ */
+public class Uzduotis9 {
+    public static void main(String[] args) {
+
+    }
+}
