53bb09e15cecff08793c2c8342e2f990ed85adad
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-11 01:49:03 +0300

    penktos uzduoties failas

---
+++ src/Uzduotis5.java
@@ -0,0 +1,12 @@
+/*
+1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
+2. Susikurti papildomą funkciją, kuri gauna int x ir patikrina ar x yra lyginis ar ne. Turėtų grąžinti true/false
+3. Susikurti papildomą funkciją, kuri per parametrus gauna List<Integer> listas ir suranda ko liste yra daugiau, ar lyginių skaičių ar nelyginių. Funkcija grąžina 1, jeigu yra daugiau lyginių, grąžina -1 jeigu nelyginių ir grąžina 0, jeigu lyginių ir nelyginių kiekis vienodas.
+4. Susikurkite dar vieną papildomą funkciją, kuri gauna per parametrus List<Integer> listas ir suranda kurių skaičių (lyginių ar nelyginių) suma yra didesnė. (tam tikriausiai reikės papildomų funkcijų, kurios randa listo lyginių sumą ir kitos funkcijos, kuri randa listo nelyginių sumą). Jeigu listo lyginių skaičių suma yra didesnė, grąžinti 1, jeigu nelyginių grąžinti -1, jeigu vienodos, tai grąžinti 0.
+5. Susikurkite dar vieną papildomą funkciją, kuri būtų "rezultato išvedimo funkcija". Funkcija per parametrus gauna List<Integer> listas, o viduje (naudojant prieš tai sukurtas funkcijas) įvertina kurių skaičių daugiau ir kurių suma didesnė ir tai parašo žodžiais. Tai pvz., jeigu viduje panaudojus 3. punkto užduotį gaunama 1 ir 4. punkto užduotį gaunama -1, tai turėtų tekstu išvesti kažką panašaus į: Liste lyginių skaičių yra daugiau, tačiau nelyginių suma yra didesnė
+ */
+public class Uzduotis5 {
+    public static void main(String[] args) {
+
+    }
+}
