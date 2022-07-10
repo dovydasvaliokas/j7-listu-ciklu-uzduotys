82c548918c0b63ceb0e3ea0c20bc0ac2e22466e0
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-11 01:24:47 +0300

    Sukurtas trecios uzduoties failass

---
+++ src/Uzduotis3.java
@@ -0,0 +1,12 @@
+/*
+1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
+2. Susikurti papildomą funkciją, kuri per parametrus gauna List<Integer> listas ir suranda visų listo neigiamų skaičių sumą.
+3. Nusikopijuokite praeitą funkciją ir ją pamodifikuokite, jog funkcija apskaičiuotų visų teigiamų skaičių sumą.
+ */
+public class Uzduotis3 {
+    public static void main(String[] args) {
+
+    }
+
+
+}
