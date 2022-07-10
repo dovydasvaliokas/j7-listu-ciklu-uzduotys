ea30c1e0021023b86085ba2ba80fca6984a50c47
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-11 02:18:08 +0300

    speitnos uzduoties failass

---
+++ src/Uzduotis7.java
@@ -0,0 +1,10 @@
+/*
+1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
+2. Susikurti papildomą funkciją, kuri gauna int x ir grąžina List<Integer> skaitmenys, kur skaitmenys tai x išskaldyti skaitmenys. Kitaip sakant, jeigu yra x = 258, tai skaitmenys[0] = 2, skaitmenys[1] = 5, skaitmenys[2] = 8. Paskutinį skaitmenį galima gauti ieškant liekanos dalinant iš 10. Paskutinį skaičių "nukąsti" galima dalinant iš 10. Būtinai reikės naudoti while ciklą, nes su for per daug nepatogu užrašyti bus (realiai sakykime neįmanoma) ARBA galima su konvertavimu į String "apeiti" šitą užduotį kitokiu būdu. Kai konvertuojate skaičių į String galite kiekvieną "simbolį" konvertuoti į skaičių ir jį dėti į int listą
+ */
+public class Uzduotis7 {
+    public static void main(String[] args) {
+
+    }
+
+}
