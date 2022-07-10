942a4c24f3322697228c5544f2c3363f338f89fa
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-11 01:01:45 +0300

    Sukurtas pirmos uzduoties fialass

---
+++ src/Uzduotis1.java
@@ -0,0 +1,12 @@
+/*
+1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
+2. Susikurti papildomą funkciją, kuri per parametrus gauna List<Integer> listas ir suranda kiek liste yra skaičių, didesnių už 10
+3. Patobulinkite tą funkciją, jog rastų kiek liste yra skaičių, didesnių už x. int x - tai funkcijos parametras. (gauna per parametrus)
+ */
+public class Uzduotis1 {
+    public static void main(String[] args) {
+
+    }
+
+
+}
