6bc0f648ab543109e7f00a4c746a4ee5c4502cb5
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-11 02:40:50 +0300

    astuntos uzduoties failas

---
+++ src/Uzduotis8.java
@@ -0,0 +1,9 @@
+/*
+1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
+2. Susikurti papildomą funkciją, kuri gauna List<Integer> skaiciai  ir grąžina didžiausio elemento INDEKSĄ (ne reikšmę o vietą KELINTAS). T.y., jeigu turime skaiciai = {109, 205, 304, 104, 205} tai rezultatas būtų 2, kadangi 304 yra didžiausias skaičius, o jo indeksas yra 2. Tai yra panašu į mano rodytą pavyzdį, tačiau skiriasi. TAČIAU, galite vos ne mano pavyzdį persidaryti, tada surasti kokia to skaičiaus yra reikšmė ir naudojant paieškos funkcijas (savo arba javos) surasti to skaičiaus indeksą.
+ */
+public class Uzduotis8 {
+    public static void main(String[] args) {
+
+    }
+}
