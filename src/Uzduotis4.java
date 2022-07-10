a5afb044d663cb20dcf756054a678d82fc08da46
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-11 01:36:11 +0300

    Sukurtass ketvirtos uzduoties failass

---
+++ src/Uzduotis4.java
@@ -0,0 +1,11 @@
+/*
+1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
+2. Nusikopijuokite funkciją iš 1 užduoties, kuri suranda kiek yra list'e skaičių, didesnių už x.
+3. Sukurkite papildomą funkciją, kuri gauna List<String> zodziai,  ir grąžintų List<Integer> zodziuIlgiai. T.y., suskaičiuotų kiekvieno žodžio ilgį ir jį įdėtų į listą, kurį po to grąžina.
+4. Naudojant 3. ir 4. užduotis suraskite kiek list'e yra žodžių, ilgesnių nei x raidžių skaičius. Tai pvz. jeigu x = 9, tai reikėtų rasti kiek liste yra žodžių, turinčių daugiau nei 9 raides/simbolius
+ */
+public class Uzduotis4 {
+    public static void main(String[] args) {
+
+    }
+}
